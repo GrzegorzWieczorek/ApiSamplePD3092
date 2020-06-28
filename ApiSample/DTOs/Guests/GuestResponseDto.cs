54b4626e2df2dfe1d6f0f4897aeeac38c3854662
GrzegorzWieczorek <wieczorekgrzegorz84@gmail.com>
2020-06-28 12:34:47 +0200

    pd3092

---
+++ ApiSample/DTOs/Guests/GuestResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using ApiSample.DTOs.Reservation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSample.DTOs.Guests
+{
+    public class GuestResponseDto
+    {
+        public GuestResponseDto()
+        {
+            Reservations = new List<ReservationResponseDto>();
+        }
+        public int IdGuest { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? DiscountPercent { get; set; }
+
+        public ICollection<ReservationResponseDto> Reservations { get; set; }
+    }
+}
